86484c7559899dfe5f8982e78da7b4691564e964
hasanbhuyan <muhammadhasanbhuyan.com>
2024-10-28 17:02:38 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-4xl mt-5 text-center font-black'>Count Down</h1>
+      <div class="diff w-[600px] h-[500px] mx-auto mt-10">
+  <div class="diff-item-1">
+    <div class="bg-primary text-primary-content grid place-content-center text-9xl font-black">
+      DAISY
+    </div>
+  </div>
+  <div class="diff-item-2">
+    <div class="bg-base-200 grid place-content-center text-9xl font-black">DAISY</div>
+  </div>
+  <div class="diff-resizer"></div>
+</div>
+
+<div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <div class="text-center lg:text-left">
+      <h1 class="text-5xl font-bold">Login now!</h1>
+      <p class="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+    </div>
+    <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <form class="card-body">
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="email" class="input input-bordered" required />
+        </div>
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="password" class="input input-bordered" required />
+          <label class="label">
+            <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div class="form-control mt-6">
+          <button class="btn btn-primary">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+    </>
+  )
+}
+
+export default App
